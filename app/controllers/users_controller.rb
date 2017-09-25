ff919ea3c6a8bd1d693ae39fa1af43d099a8142a
1412320 <quocminh97@gmail.com>
2017-09-26 00:13:28 +0700

    Initialize layout with bootstrap

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def home
+  end
+
+  def albums
+  end
+
+  def blog
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#home'
+  get '/', to: 'users#home'
+  
+  get 'users/home'
+
+  get 'users/albums'
+
+  get 'users/blog'
+
+  get 'users/about'
 end
