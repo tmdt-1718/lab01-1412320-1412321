9411f1393d10edf0067566157b41fb782cd7bc72
1412321 <minhluong96@gmail.com>
2017-09-25 22:31:41 +0700

    Add Basic User Model with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   get 'users/blog'
 
   get 'users/about'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create([{ email: "minhluong96@gmail.com", password: "1234567", confirmed_at: DateTime.now, }])
