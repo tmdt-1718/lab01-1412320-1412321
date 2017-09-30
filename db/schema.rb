ccf71bccf78f84964115e3e9b00a50862c4df540
1412320 <quocminh97@gmail.com>
2017-09-30 11:54:06 +0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170929141253) do
-
-  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "url"
-    t.integer "views"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_images_on_user_id"
+ActiveRecord::Schema.define(version: 20170929142130) do
 
   create_table "blogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "content"
@@ -28,6 +20,15 @@ ActiveRecord::Schema.define(version: 20170929141253) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "url"
+    t.integer "views"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -52,6 +53,6 @@ ActiveRecord::Schema.define(version: 20170929141253) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "images", "users"
   add_foreign_key "blogs", "users"
+  add_foreign_key "images", "users"
 end
