ae821832184f5c5de875f07a914b480d8c424a45
Luong Nhat Minh <minhluong@teqnological.asia>
2017-09-30 17:54:29 +0700

    Add Show Album Action

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,10 +1,11 @@
 class AlbumsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @albums = Album.find_by(user_id: params[:user_id])
+    @albums = Album.where(user_id: params[:user_id])
   end
 
   def show
-    
+    @album = Album.find(params[:id])
+    @images = @album.images
   end
 end
