d1bacdb7a37450c2f0a4cdbbbffe99c2ad554a63
Luong Nhat Minh <minhluong@teqnological.asia>
2017-10-02 14:17:21 +0700

    Authorize Image delete, change cover

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
   $("#modal").on('show.bs.modal', function (event) {
     var img = $(event.relatedTarget) //image that trigger modal
     takeImageInfo(img.data("id"))
@@ -10,20 +10,16 @@ function takeImageInfo(id){
   $.get("/images/" + id).done(function(result){
     var imgDes = $("#modalImg")
     imgDes.attr("src", result.image.url.url);
-    $("#modalContent").html(
-    gravatar(result.user, 60) + 
-    '<h5><a href="/users/'+ result.user.id +'">'
-    + result.user.name + '</a></h5> '+
-    '<div>Views: '+ result.image.views +'</div>' +
-    '<form method="post" id="album-cover" action="/users/' + result.user.id +
-    '/albums/' + result.image.album_id + '/cover">' +
-    '<input type="hidden" name="image_id" value="' + result.image.id +'">' +
-    '</form>' + '<a href="#" id="changeCover">Set album cover</a>' )
+    $("#modalContent").html(result.html)
     $("#changeCover").click(function(){
       $("#album-cover").submit();
     })
+    $("#triggerDelete").click(function(){
+      return confirm("Are really want to delete this photo?");
+    })
   })
 }
+
 
 function gravatar(user, size){
   gravatar_id = $.md5(user.email.toLowerCase());
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :images, only: [ :show, :index ]
+  resources :images, only: [ :show, :index, :destroy ]
   resources :users, only: [ :member ] do
     resources :blogs, only: [ :index, :show ]
     resources :albums, only: [ :index, :show, :member, :cover ] do
