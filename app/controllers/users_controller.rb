716d4922ceb45b7396f7235e71a915f817f6c741
1412320 <quocminh97@gmail.com>
2017-10-05 12:04:20 +0700

    Add home page and modified sidebar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def home
+    @user = User.find(params[:user_id])
+    @blog = Blog.where(user_id: params[:user_id])
+    @album = Album.where(user_id: params[:user_id])
   end
 
   def albums
