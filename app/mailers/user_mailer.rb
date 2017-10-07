cb1248768b006a2c43bbb03217e51723bcf5f536
1412321 <minhluong96@gmail.com>
2017-10-07 17:25:26 +0700

    Sent mail when user logged in

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.login.subject
+  #
+  def login(user)
+    @user = user
+    mail to: user.email
+  end
+end
